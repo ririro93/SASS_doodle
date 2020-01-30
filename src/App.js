2740ca639a30ba348fec783a28acc92c4e0bd63e
root <ririro93@gmail.com>
2020-01-30 14:17:19 +0000

    day1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    	<main className="App">
+			<div className="box red">
+				1번째 div
+			</div>
+			<div className="box blue">
+				2번째 div
+			</div>
+			<div className="box orange">
+				3번째 div
+			</div>
+		</main>
     );
   }
 }
