6e38b59d45616bc7fe73d7ccda24bc9b11faed2e
root <ririro93@gmail.com>
2020-01-30 15:02:03 +0000

    play with click events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-    	<main className="App">
-			<div className="box red">
-				1번째 div
-			</div>
-			<div className="box blue">
-				2번째 div
-			</div>
-			<div className="box orange">
-				3번째 div
-			</div>
-		</main>
-    );
+	state = {
+		red: false,
+		blue: false,
+		orange: false,
+	}
+	
+	handleClick = (e) => {
+		let bool = (e.target.value == 'true');
+		this.setState({
+			[e.target.name]: !bool
+		})
+	}
+
+	render() {
+		console.log(this.state);
+		const { red, blue, orange } = this.state;
+		
+		return (
+			<main className="App">
+				<button 
+					className={classNames('box', {red})}
+					onClick={this.handleClick}
+					name="red"
+					value={red}
+				>
+					{red}
+				</button>
+				<button 
+					className={classNames('box', {blue})}
+					onClick={this.handleClick}
+					name="blue"
+					value={blue}
+				>
+					{blue}
+				</button>
+				<button 
+					className={classNames('box', {orange})}
+					onClick={this.handleClick}
+					name="orange"
+					value={orange}
+				>
+					{orange}
+				</button>
+			</main>
+		);
   }
 }
 
